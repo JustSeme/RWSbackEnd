44d2dca70ab3df4e2ce0609ace7d07293e1644df
JustSemen <semyn03@mail.ru>
2023-01-03 17:23:43 +0600

    rest api

--- 03/src/index.ts
+++ 03/src/index.ts
@@ -1,29 +1,87 @@
 import express from 'express'
 
 const app = express()
-const port = 3000 
+const port = 3000
+const jsonBodyMiddleWare = express.json()
+app.use(jsonBodyMiddleWare)
 
-app.get('/', (req, res) => {
-    res.send('Hello World')
+const HTTP_STATUSES = {
+    OK_200: 200,
+    CREATED_201: 201,
+    NO_CONTENT_204: 204,
+
+    NOT_FOUND_404: 404,
+    BAD_REQUEST_400: 400,
+}
+
+const db = {
+    courses: [
+        { id: 1, title: 'front-end' },
+        { id: 2, title: 'back-end' },
+        { id: 3, title: 'fullstack' },
+        { id: 4, title: 'dev-ops' }
+    ]
+}
+
+app.get('/courses', (req, res) => {
+    const foundCourses = req.query.title ? db.courses
+        .filter(c => c.title.indexOf(req.query.title as string) > -1)
+        : db.courses
+
+    res.json(foundCourses)
+})
+app.get('/courses/:id', (req, res) => {
+    const foundCourses = db.courses.find(course => course.id === +req.params.id)
+
+    if(!foundCourses) {
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
+        return
+    }
+
+    res.json(foundCourses)
+})
+app.post('/courses', (req, res) => {
+    if(req.body.title.length < 3) {
+        res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400)
+        return
+    }
+
+    const createdCourse = {
+        id: +(new Date()),
+        title: req.body.title
+    }
+    db.courses.push(createdCourse)
+
+    res.status(HTTP_STATUSES.CREATED_201)
+        .json(createdCourse)
+})
+app.delete('/courses/:id', (req, res) => {
+    let dbLength = db.courses.length
+    db.courses = db.courses.filter(course => course.id !== +req.params.id)
+
+    if(dbLength === db.courses.length) {
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
+    }
+
+    res.sendStatus(HTTP_STATUSES.NO_CONTENT_204)
 })
 
-app.get('/samurais', (req, res) => {
-    const a = 4
-    if (a > 5) {
-        res.send('OK')
-    } else {
-        res.send('Hello, samurais!')
+app.put('/courses/:id', (req, res) => {
+    if(req.body.title.length < 3) {
+        res.sendStatus(HTTP_STATUSES.BAD_REQUEST_400)
+        return
     }
-})
 
-app.post('/samurais', (req, res) => {
-    const response = {
-        data: {
-            message: 'We have created samurai'
-        },
-        statusCode: 0
+    let foundCourses = db.courses.find(course => course.id === +req.params.id)
+
+    if(!foundCourses) {
+        res.sendStatus(HTTP_STATUSES.NOT_FOUND_404)
+        return
     }
-    res.send(JSON.stringify(response))
+
+    foundCourses.title = req.body.title
+
+    res.json(foundCourses)
 })
 
 app.listen(port, () => {
